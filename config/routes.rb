26e132cda32424eddf8b50cc9f95ccf7b028fae2
taro.ishii <taroishii31@icloud.com>
2018-11-14 08:49:21 +0000

    task

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
-  resources :tasks
+  resources :tasks, only: [:create, :destroy]
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
